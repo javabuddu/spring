86f91fa90679b4a2cb73f0ac4b774698265f1624
javabuddu <buddu44@gmail.com>
2022-12-15 19:57:07 +0530

    create repositry

---
+++ springproj1example-file-upload/src/main/java/com/spring/file/Repositry/FileRepositry.java
@@ -0,0 +1,7 @@
+package com.spring.file.Repositry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileRepositry extends JpaRepository<Employee, long> {
+
+}
--- springproj1example-file-upload/src/main/java/com/spring/file/Repositry/fileRepositry.java
+++
@@ -1,5 +0,0 @@
-package com.spring.file.Repositry;
-
-public interface fileRepositry {
-
-}
